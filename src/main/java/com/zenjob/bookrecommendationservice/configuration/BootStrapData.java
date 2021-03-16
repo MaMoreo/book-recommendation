f7ef7623a4150cfd69604bee8db8131489bfb4b9
Miguel Moreo <miguelangel.moreo@gmail.com>
2021-03-16 20:54:33 +0100

    Add user for testing

--- src/main/java/com/zenjob/bookrecommendationservice/configuration/BootStrapData.java
+++ src/main/java/com/zenjob/bookrecommendationservice/configuration/BootStrapData.java
@@ -6,19 +6,24 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.zenjob.bookrecommendationservice.entity.Book;
+import com.zenjob.bookrecommendationservice.entity.User;
 import com.zenjob.bookrecommendationservice.model.CSVFileReader;
 import com.zenjob.bookrecommendationservice.repository.BookRepository;
+import com.zenjob.bookrecommendationservice.repository.UserRepository;
 
 @Component
 public class BootStrapData implements CommandLineRunner {
 
 	private final BookRepository bookRepository;
+	private final UserRepository userRepository;
+	
 	private final CSVFileReader csv;
 	
-	public BootStrapData(CSVFileReader csv, BookRepository bookRepository) {
+	public BootStrapData(CSVFileReader csv, BookRepository bookRepository, UserRepository userRepository) {
 		super();
 		this.csv = csv;
 		this.bookRepository = bookRepository;
+		this.userRepository = userRepository;
 	}
 
 
@@ -26,5 +31,7 @@ public class BootStrapData implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		List<Book> books = csv.processBooks();
 		books.stream().forEach(b -> bookRepository.save(b));
+		
+		userRepository.save(new User("Mike"));
 	}
 }
